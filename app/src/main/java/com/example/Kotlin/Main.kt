8443626fcfed615781b7f5208c89815234587eab
Musfiq <mushfiqur15-10872@diu.edu.bd>
2021-11-07 22:22:30 +0600

    Variable

---
+++ app/src/main/java/com/example/Kotlin/Main.kt
@@ -0,0 +1,17 @@
+package com.example.Kotlin
+
+fun main(args: Array<String>){
+    println("Hello world")
+
+    var sayed: String = "Musfiqur Rahman"
+    println(sayed)
+
+    var sayedSalary: Int = 32
+    var monthly: Int = sayedSalary*4
+    println(monthly)
+    println(sayedSalary)
+
+    val weeks: Int = 130
+    val years: Double = weeks/52.0
+    println(years)
+}
