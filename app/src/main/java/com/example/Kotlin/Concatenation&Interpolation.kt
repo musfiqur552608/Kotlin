71b40a03e699f4ac0dbf26e70a53ca29bdd30f25
Musfiq <mushfiqur15-10872@diu.edu.bd>
2021-11-08 10:06:08 +0600

    Concatenation

---
+++ app/src/main/java/com/example/Kotlin/Concatenation&Interpolation.kt
@@ -0,0 +1,13 @@
+package com.example.Kotlin
+
+fun main(args: Array<String>){
+    var sayed: String = "Musfiqr Rahman"
+
+    println("My name is: "+sayed)
+
+    val apples: Int = 6
+    val oranges: Int = 5
+    var fruit: Int = apples - oranges
+    println(fruit)
+    println("A Quater of The Apples is ${apples/4}")
+}
