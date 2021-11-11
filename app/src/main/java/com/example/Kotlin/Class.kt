a99c78267a80264e6321e88fe4736bff9d1d1c2c
Musfiq <mushfiqur15-10872@diu.edu.bd>
2021-11-11 21:22:38 +0600

    Class

--- app/src/main/java/com/example/Kotlin/Class.kt
+++ app/src/main/java/com/example/Kotlin/Class.kt
@@ -1,7 +1,7 @@
 package com.example.Kotlin
 
 fun main(args: Array<String>){
-    val sayed = Player("Sayed")
+    val sayed = Player("Sayed",4,8, 0)
 //    println(sayed.name)
 //    println(sayed.lives)
 //    println(sayed.level)
@@ -9,9 +9,9 @@ fun main(args: Array<String>){
 
     sayed.show()
 
-    val sakib = Player("Sakib")
-    sakib.level = 5
+    val sakib = Player("Sakib", 5, 10, 1000)
     sakib.show()
+
 
 
 }
--- app/src/main/java/com/example/Kotlin/Player.kt
+++ app/src/main/java/com/example/Kotlin/Player.kt
@@ -1,10 +1,12 @@
 package com.example.Kotlin
 
-class Player(val name: String) {
-    var lives = 3;
-    var level = 1;
-    var score = 0;
+//class Player(val name: String) {
+//    var lives = 3;
+//    var level = 1;
+//    var score = 0;
 
+class Player(val name:String, var lives:Int, var level:Int, var score:Int)
+{
     fun show(){
         println("""
             name: $name
